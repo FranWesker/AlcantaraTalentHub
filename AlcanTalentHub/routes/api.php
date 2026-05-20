483b52c2653ebe2b15cffa0674e8d5926b0fb24b
FranSparda <javierfran2409@gmail.com>
2026-05-20 17:43:11 +0200

    Eliminar el cierre (closure) /user mal ubicado y la ruta anidada.
    Se ha eliminado un cierre (closure) de ruta /user definido incorrectamente en routes/api.php, el cual contenía una ruta anidada Route::get('/offers/search') y devolvía $request->user() mediante auth:sanctum. Se eliminó el registro de dicha ruta anidada para limpiar el archivo de rutas de la API, permitiendo así que la ruta de búsqueda de ofertas (offers search) sea definida correctamente al nivel principal (top level).

--- AlcanTalentHub/routes/api.php
+++ AlcanTalentHub/routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SearchController;
 
-Route::get('/user', function (Request $request) {
-    // Ruta a la que llamará nuestro JavaScript: /api/offers/search?q=termino
-    Route::get('/offers/search', [SearchController::class, 'searchOffers']);
-    return $request->user();
-})->middleware('auth:sanctum');
+
